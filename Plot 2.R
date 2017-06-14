17795977388c80bd0662060e90efeb20a7274ea4
willskemp <willskemp@gmail.com>
2017-06-14 12:39:30 +0100

    Add files via upload

---
+++ Plot 2.R
@@ -0,0 +1,25 @@
+# Load 'lattice' package
+require(lattice)
+
+# Set working directory to file location
+setwd("C:/Users/william.kemp/Desktop/Data Science - John Hopkins/4. Exploratory Data Analysis/Week 1 Peer Review/Dataset")
+
+# Read in the data file with ";" separator
+plotdata <-  read.table("household_power_consumption.txt",sep=";",header =T,
+                        na.strings ="?")
+
+# Convert date and time data to "POSIXct" format. Create new merged DateTime variable. 
+plotdata$DateTime <- with(plotdata,as.POSIXct(paste(Date,Time),format="%d/%m/%Y %H:%M:%S"))
+plotdata$Date <- as.Date(plotdata$Date,format ="%d/%m/%Y")
+plotdata$Time <- strptime(plotdata$Time,format="%H:%M:%S")
+
+
+# Create new dataset called 'subdata'
+#This will consists of plotdata filtered to the data range 2007-02-01 and 2007-02-02
+subdata <- subset(plotdata,Date>="2007-02-01" & Date <="2007-02-02")
+
+# Plot 2
+png(filename = "Plot2.png", height = 480,width =480)
+plot(subdata$DateTime,subdata$Global_active_power,type="l",
+     xlab="",ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ Plot3.R
@@ -0,0 +1,30 @@
+# Load 'lattice' package
+require(lattice)
+
+# Set working directory to file location
+setwd("C:/Users/william.kemp/Desktop/Data Science - John Hopkins/4. Exploratory Data Analysis/Week 1 Peer Review/Dataset")
+
+# Read in the data file with ";" separator
+plotdata <-  read.table("household_power_consumption.txt",sep=";",header =T,
+                        na.strings ="?")
+
+# Convert date and time data to "POSIXct" format. Create new merged DateTime variable. 
+plotdata$DateTime <- with(plotdata,as.POSIXct(paste(Date,Time),format="%d/%m/%Y %H:%M:%S"))
+plotdata$Date <- as.Date(plotdata$Date,format ="%d/%m/%Y")
+plotdata$Time <- strptime(plotdata$Time,format="%H:%M:%S")
+
+
+# Create new dataset called 'subdata'
+#This will consists of plotdata filtered to the data range 2007-02-01 and 2007-02-02
+subdata <- subset(plotdata,Date>="2007-02-01" & Date <="2007-02-02")
+
+
+# Plot 3
+png(filename ="Plot3.png", height = 480,width =480)
+plot(subdata$DateTime,subdata$Sub_metering_1,type="l",
+     xlab="",ylab="Energy sub metering")
+lines(subdata$DateTime,subdata$Sub_metering_2,col = 2)
+lines(subdata$DateTime,subdata$Sub_metering_3,col = 4)
+legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_3"),
+       lwd=c(2.5,2.5,2.5),col= c(1,2,4),cex = 0.5)
+dev.off()
